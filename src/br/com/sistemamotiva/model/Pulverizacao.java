b48dd5fb5c18da722634c68fc8c5313c1fc0de58
manoela-oliveira <manoela.oliveira0900@gmail.com>
2026-09-16 23:35:22 -0300

    feat: ajusta RocadaMecanizada e Pulverizacao conforme ids

--- src/br/com/sistemamotiva/model/Pulverizacao.java
+++ src/br/com/sistemamotiva/model/Pulverizacao.java
@@ -1,12 +1,24 @@
 package br.com.sistemamotiva.model;
 
-public class Pulverizacao extends IntervencaoOperacionalDAO {
+public class Pulverizacao extends IntervencaoOperacional {
+
+    public Pulverizacao() {
+        super("Pulverização Química Preventiva", 850.0);
+    }
+
+    public Pulverizacao(String descricao, double custo) {
+        super(descricao, custo);
+    }
 
     @Override
-    public void executarServico(TrechoRodoviaDAO trechoAlvo) {
-        // Pulverização química preventiva reduz o nível de vegetação para 8cm
+    public String getTipo() {
+        return "PULVERIZACAO";
+    }
+
+    @Override
+    public void executarServico(TrechoRodovia trechoAlvo) {
         trechoAlvo.atualizarNivelVegetacao(8.0);
-        System.out.println("[Serviço] Pulverização Química de inibição concluída no trecho: " 
+        System.out.println("[Serviço] Pulverização Química concluída no trecho: " 
             + trechoAlvo.getIdentificador().getCodigoIdentificacao());
     }
 }
--- src/br/com/sistemamotiva/model/RocadaMecanizada.java
+++ src/br/com/sistemamotiva/model/RocadaMecanizada.java
@@ -1,9 +1,22 @@
 package br.com.sistemamotiva.model;
 
-public class RocadaMecanizada extends IntervencaoOperacionalDAO {
+public class RocadaMecanizada extends IntervencaoOperacional {
+
+    public RocadaMecanizada() {
+        super("Roçada Mecânica Pesada", 1500.0);
+    }
+
+    public RocadaMecanizada(String descricao, double custo) {
+        super(descricao, custo);
+    }
 
     @Override
-    public void executarServico(TrechoRodoviaDAO trechoAlvo) {
+    public String getTipo() {
+        return "ROCADA";
+    }
+
+    @Override
+    public void executarServico(TrechoRodovia trechoAlvo) {
         trechoAlvo.atualizarNivelVegetacao(5.0);
         System.out.println("[Serviço] Roçada Mecanizada concluída no trecho: " 
             + trechoAlvo.getIdentificador().getCodigoIdentificacao());
