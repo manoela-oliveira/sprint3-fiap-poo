482a93ed9324f53ba46a38072e07f23f650bf611
manoela-oliveira <manoela.oliveira0900@gmail.com>
2026-09-16 23:31:57 -0300

    feat: adiciona ids para persistencia de dados

--- src/br/com/sistemamotiva/model/IntervencaoOperacional.java
+++ src/br/com/sistemamotiva/model/IntervencaoOperacional.java
@@ -1,8 +1,31 @@
 package br.com.sistemamotiva.model;
-/*Esta classe é abstrata porque se trata de um termo genérico que não deve existir sozinho, visto 
-  que não é possível instanciar um "Serviço Genérico" em campo no novo sistema, apenas ações reais 
-  como Roçar ou Pulverizar.*/
 
 public abstract class IntervencaoOperacional {
+    private Long id;
+    private String descricao;
+    private double custoEstimado;
+
+    public IntervencaoOperacional() {}
+
+    public IntervencaoOperacional(String descricao, double custoEstimado) {
+        this.descricao = descricao;
+        this.custoEstimado = custoEstimado;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public double getCustoEstimado() { return custoEstimado; }
+    public void setCustoEstimado(double custoEstimado) { this.custoEstimado = custoEstimado; }
+
+    public abstract String getTipo();
     public abstract void executarServico(TrechoRodovia trechoAlvo);
+
+    @Override
+    public String toString() {
+        return String.format("Intervencao[id=%d, tipo=%s, desc=%s, custo=R$%.2f]", id, getTipo(), descricao, custoEstimado);
+    }
 }
